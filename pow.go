7b4308706fa2fc7f792d28095f0a4d54055d28ec
astromonk <32324002+OhJaeSong@users.noreply.github.com>
2020-12-14 00:08:43 +0900

    Add files via upload

---
+++ pow.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"bytes"
+	"crypto/sha256"
+	"fmt"
+	"math"
+	"math/big"
+)
+
+var (
+	maxNonce = math.MaxInt64
+)
+
+const targetBits = 4
+
+type ProofOfWork struct {
+	block  *Block
+	target *big.Int
+}
+
+func NewProofOfWork(b *Block) *ProofOfWork {
+	target := big.NewInt(1)
+	target.Lsh(target, uint(256-targetBits))
+
+	pow := &ProofOfWork{b, target}
+
+	return pow
+}
+
+func (pow *ProofOfWork) prepareData(nonce int) []byte {
+	data := bytes.Join(
+		[][]byte{
+			pow.block.PrevBlockHash,
+			pow.block.HashTransactions(),
+			IntToHex(pow.block.Timestamp),
+			IntToHex(int64(targetBits)),
+			IntToHex(int64(nonce)),
+		},
+		[]byte{},
+	)
+
+	return data
+}
+
+func (pow *ProofOfWork) Run() (int, []byte) {
+	var hashInt big.Int
+	var hash []byte
+
+	nonce := 0
+
+	for nonce; nonce <= maxNonce; nonce++ {
+		hashInt := prepareData(nonce)
+
+		if (hashInt.Cmp(target) > 0){
+			break;
+		}
+	}
+
+	return (nonce, hashInt)
+}
+
+func (pow *ProofOfWork) Validate() bool {
+	var hashInt big.Int
+
+	data := pow.prepareData(pow.block.Nonce)
+	hash := sha256.Sum256(data)
+	hashInt.SetBytes(hash[:])
+
+	isValid := hashInt.Cmp(pow.target) == -1
+
+	return isValid
+}
